f57128d6da7cf8616e30e460b625f97eb5ce4e5d
Karan <kpahawa@deloitte.com>
2019-01-10 02:48:50 -0600

    added config file

---
+++ frontend/src/app/config/config.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class Config {
+    private readonly dev = {
+        serverAndProtocol: '127.0.0.1',
+        port: 8205,
+        // set dev parent URL here
+        parentURL: "http://127.0.0.1:8201",
+        urlPrefix: null
+    };
+   
+    private readonly prod = {
+        serverAndProtocol: '92f16d81.ngrok.io/',
+        port: null,
+        // set dev parent URL here
+        parentURL: 'http://localhost:4200',
+        urlPrefix: null
+    };
+    public readonly ActiveEnvironment = this.prod;
+} 
