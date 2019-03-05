a8447f051507ac1f57e787615b467b209b3eda07
Pahawa, Karan <Karan.Pahawa@uscellular.com>
2019-03-05 00:41:40 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,13 @@
 
 echo Doing project set up
 
+echo Doing ng build
+pushd frontend/
+ng build
+popd
+
 echo collecting static now
-exec python3.6 manage.py collectstatic
+exec sudo python3.6 manage.py collectstatic
 
 echo Starting django server
 exec python3.6 manage.py runserver 8201
