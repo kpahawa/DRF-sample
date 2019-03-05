50d2796f6000854d3aa302f812d7b6ff6368eacd
Pahawa, Karan <Karan.Pahawa@uscellular.com>
2019-03-05 10:57:28 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo Doing project set up
-
-echo Doing ng build
-pushd frontend/
-npm install
-ng build
-popd
-
 echo Starting django server
 exec python3.6 manage.py runserver 0.0.0.0:8201
 
